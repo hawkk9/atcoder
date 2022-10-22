7185ce00b96236b7eb1b492e6065392f73b078fa
yuichi-nishitani <yuichi.nishitani@naviplus.co.jp>
2022-10-22 14:20:05 +0900

    pg3

--- pg-battle/2022/3.rb
+++ pg-battle/2022/3.rb
@@ -1,22 +1,25 @@
+def count_grater_score(sorted_uniq_scores, group_by_scores, i)
+  return 0 if i == 0
+  group_by_scores[sorted_uniq_scores[i - 1]] + count_grater_score(sorted_uniq_scores, group_by_scores, i - 1)
+end
+
 N, p, q = gets.chomp.split.map(&:to_i)
 
 scores = []
-pointers = []
-N.times do |i|
+N.times do
   a, b = gets.chomp.split.map(&:to_i)
   score = p * a + q * b
-  score_obj = { score: score }
-  pointers[i] = score_obj
-  scores.push(score_obj)
+  scores.push(score)
 end
 
-p scores.sort{|a, b| a[:score] <=> b[:score] }
+sorted_scores = scores.sort{|a, b| b <=> a }
+sorted_uniq_scores = sorted_scores.uniq
+group_by_scores = sorted_scores.tally
+rank = {}
+sorted_uniq_scores.each_with_index do |score, i|
+  rank[score] = count_grater_score(sorted_uniq_scores, group_by_scores, i) + 1
+end
 
-# 順位を記録
 scores.each do |score|
-
-end
-
-N.times do |i|
-  p pointers[i]
+  puts rank[score]
 end
