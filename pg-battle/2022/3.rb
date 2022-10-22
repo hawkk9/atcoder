074482ac44fd4f88e8b3f785fd2cee1cd0350fd0
yuichi-nishitani <yuichi.nishitani@naviplus.co.jp>
2022-10-22 14:25:34 +0900

    pg3-cache

--- pg-battle/2022/3.rb
+++ pg-battle/2022/3.rb
@@ -1,6 +1,11 @@
-def count_grater_score(sorted_uniq_scores, group_by_scores, i)
+@count_cache = {}
+
+def count_grater_score(i)
   return 0 if i == 0
-  group_by_scores[sorted_uniq_scores[i - 1]] + count_grater_score(sorted_uniq_scores, group_by_scores, i - 1)
+  return @count_cache[i] unless @count_cache[i].nil?
+  count = @group_by_scores[@sorted_uniq_scores[i - 1]] + count_grater_score(i - 1)
+  @count_cache[i] = count
+  count
 end
 
 N, p, q = gets.chomp.split.map(&:to_i)
@@ -13,11 +18,11 @@ N.times do
 end
 
 sorted_scores = scores.sort{|a, b| b <=> a }
-sorted_uniq_scores = sorted_scores.uniq
-group_by_scores = sorted_scores.tally
+@sorted_uniq_scores = sorted_scores.uniq
+@group_by_scores = sorted_scores.tally
 rank = {}
-sorted_uniq_scores.each_with_index do |score, i|
-  rank[score] = count_grater_score(sorted_uniq_scores, group_by_scores, i) + 1
+@sorted_uniq_scores.each_with_index do |score, i|
+  rank[score] = count_grater_score(i) + 1
 end
 
 scores.each do |score|
