e6485871ca216a3c5e05ab4e58a4889900b733b3
yuichi-nishitani <yuichi.nishitani@naviplus.co.jp>
2022-10-22 13:45:12 +0900

    wip

--- pg-battle/2022/3.rb
+++ pg-battle/2022/3.rb
@@ -1,10 +1,22 @@
 N, p, q = gets.chomp.split.map(&:to_i)
 
 scores = []
+pointers = []
 N.times do |i|
   a, b = gets.chomp.split.map(&:to_i)
   score = p * a + q * b
-  scores.push(score)
+  score_obj = { score: score }
+  pointers[i] = score_obj
+  scores.push(score_obj)
 end
 
-p scores
+p scores.sort{|a, b| a[:score] <=> b[:score] }
+
+# 順位を記録
+scores.each do |score|
+
+end
+
+N.times do |i|
+  p pointers[i]
+end
