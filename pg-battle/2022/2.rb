759fec7df260a67a077c4230b7c1aeec7cce7f39
yuichi-nishitani <yuichi.nishitani@naviplus.co.jp>
2022-10-22 13:23:18 +0900

    pg2

--- pg-battle/2022/2.rb
+++ pg-battle/2022/2.rb
@@ -1,1 +1,12 @@
+N = gets.chomp.to_i
+formula = gets.chomp
 
+groups =  formula.delete('+').split('=').map do |group|
+  group.strip.split.map(&:to_i)
+end
+sum_by_group = groups.map do |group|
+  group.sum
+end
+result = sum_by_group.uniq.count == 1
+
+puts result ? 'Yes' : 'No'
