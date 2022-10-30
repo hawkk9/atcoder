183c9db2a5a76908bc117e991f430e230b69c501
yuichi-nishitani <yuichi.nishitani@naviplus.co.jp>
2022-10-30 14:54:26 +0900

    RE、TLE

---
+++ atcoder/typical90/076.rb
@@ -0,0 +1,27 @@
+def get_size(i)
+  i != N ? A[i] : A[0]
+end
+
+N = gets.chomp.to_i
+A = gets.chomp.split.map(&:to_i)
+
+hole_size = A.sum
+target_size = hole_size / 10
+
+result = false
+N.times do |start_i|
+  sub_total = A[start_i]
+
+  i = 1
+  while sub_total < target_size do
+    sub_total += get_size(start_i + i)
+    i += 1
+  end
+
+  if sub_total == target_size
+    result = true
+    break
+  end
+end
+
+puts result ? 'Yes' : 'No'
