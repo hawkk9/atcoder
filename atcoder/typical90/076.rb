0f0fdbf35c19be162fffde061f294cc47e853cd2
yuichi-nishitani <yuichi.nishitani@naviplus.co.jp>
2022-11-03 11:20:54 +0900

    円環状、解答見て回答

--- atcoder/typical90/076.rb
+++ atcoder/typical90/076.rb
@@ -1,27 +1,28 @@
-def get_size(i)
-  i < N ? A[i] : A[i - N]
-end
-
 N = gets.chomp.to_i
 A = gets.chomp.split.map(&:to_i)
 
 hole_size = A.sum
-target_size = hole_size / 10
+target_size = hole_size / 10.0
 
+cuts = [0]
+(1..N).each do |i|
+  cuts[i] = cuts[i - 1] + A[i - 1]
+end
+(1..N).each do |i|
+  cuts[i + N] = cuts[i + N - 1] + A[i - 1]
+end
+
+# R - L = target_size
+# R = target_size + L
+
+# p cuts
 result = false
-N.times do |start_i|
-  sub_total = A[start_i]
-
-  i = 1
-  while sub_total < target_size do
-    sub_total += get_size(start_i + i)
-    i += 1
-  end
-
-  if sub_total == target_size
-    result = true
-    break
-  end
+N.times do |l|
+  target = cuts[l] + target_size
+  hit = cuts.bsearch{|cut| cut >= target }
+  # puts "L=#{l}, target=#{target}, hit=#{hit}"
+  result = hit != nil && hit == target
+  break if result
 end
 
 puts result ? 'Yes' : 'No'
